32f22fc6073c326b0ade4c7e7cbfcdb62f33aa2b
BijanProgrammer <bijaneisapour@gmail.com>
2022-09-19 15:06:29 +0430

    [INIT]

---
+++ Portal/Database/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portal.Database;
+
+public class Context : DbContext
+{
+    public DbSet<RawDbSet> Raw { get; set; }
+    public DbSet<IgnoreMe> IgnoreMe { get; set; }
+
+    public Context(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RawDbSet>().HasNoKey();
+    }
+}
+
+public class RawDbSet
+{
+}
+
+public class IgnoreMe
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
---
+++ Portal/Migrations/20220919100637_Init.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portal.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Raw",
+                columns: table => new
+                {
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Raw");
+        }
+    }
+}
